1f6acb2dd28785baa9e489555a5156ea8b33e09a
panduramadhan2 <panduramadhan2@gmail.com>
2024-08-26 10:19:45 +0700

    checkpoint mengedit product 29

--- server/routes/ProductRoutes.js
+++ server/routes/ProductRoutes.js
@@ -62,4 +62,35 @@ router.delete("/delete/:id", async (req, res) => {
   }
 });
 
+router.put("/update/:id", async (req, res) => {
+  try {
+    let product = await Product.findById(req.params.id);
+
+    const { name, desc, category, price, capital, stock, weight } = req.body;
+
+    const profit = price - capital;
+
+    const data = {
+      name: name,
+      desc: desc,
+      category: category,
+      price: price,
+      capital: capital,
+      profit: profit,
+      stock: stock,
+      weight: weight,
+    };
+    product = await Product.findByIdAndUpdate(req.params.id, data, {
+      new: true,
+      runValidators: true,
+    });
+    res.status(200).json({ message: "Produk berhasil diperbarui" });
+  } catch (error) {
+    if (error.name === "CastError") {
+      return res.status(404).json({ error: "Produk tidak ditemukan" });
+    }
+    return res.status(500).json({ error: error.message });
+  }
+});
+
 export default router;
