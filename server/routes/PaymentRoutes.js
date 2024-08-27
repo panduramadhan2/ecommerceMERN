510e08a4e88dff602b1dcd1070b2120db3e2abfd
panduramadhan2 <panduramadhan2@gmail.com>
2024-08-27 15:51:37 +0700

    checkpoint status pembayaran 35

--- server/routes/PaymentRoutes.js
+++ server/routes/PaymentRoutes.js
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { response } from "express";
 import midtransClient from "midtrans-client";
 import { authenticate } from "../middleware/authenticate.js";
 
@@ -55,6 +55,63 @@ router.post(
         });
     } catch (error) {
       return res.status(500).json({ error: error.message });
+    }
+  }
+);
+
+// router.get(
+//   "status/:orderId",
+//   authenticate(["admin", "user"]),
+//   async (req, res) => {
+//     try {
+//       const snap = new midtransClient.Snap({
+//         isProduction: false,
+//         serverKey: process.env.SERVER_KEY,
+//         clientKey: process.env.CLIENT_KEY,
+//       });
+
+//       snap.transaction
+//         .status(req.params.orderId)
+//         .then((response) => {
+//           res.status(200).json(response);
+//         })
+//         .catch((error) => {
+//           res.status(404).json({ error: "Order tidak ditemukan" });
+//         });
+//     } catch (error) {
+//       return res.status(500).json({ error: error.message });
+//     }
+//   }
+// );
+
+router.get(
+  "/status/:orderId",
+  authenticate(["admin", "user"]),
+  async (req, res) => {
+    try {
+      const snap = new midtransClient.Snap({
+        isProduction: false,
+        serverKey: process.env.SERVER_KEY,
+        clientKey: process.env.CLIENT_KEY,
+      });
+
+      //   const orderId = req.params.orderId;
+
+      // Format or validate orderId if needed
+
+      const response = await snap.transaction
+        .status(req.params.orderId)
+        .then((response) => response); // Return response object
+
+      return res.status(200).json(response); // Return response to client
+    } catch (error) {
+      if (error.statusCode === 404) {
+        // Example: Specific error handling
+        return res.status(404).json({ error: "Order not found" });
+      } else {
+        // Handle other error codes or general errors
+        return res.status(500).json({ error: error.message });
+      }
     }
   }
 );
